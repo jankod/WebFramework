f17e2f2c83b50d81b5a93e2e54206e52365c3859
Janko Diminic <janko.diminic@sedamit.hr>
2019-07-08 00:23:40 +0200

    all

--- src-js/hr/Action.java
+++ src-js/hr/Action.java
@@ -5,6 +5,6 @@ public class Action {
 	
 	public static class User {
 		public Long id = 22L;
-	}
+	}	
 	
 }
--- src/main/resources/public/generated/hr/Action.js
+++ src/main/resources/public/generated/hr/Action.js
@@ -1,17 +1,14 @@
-/* Generated from Java with JSweet 2.3.0-SNAPSHOT - http://www.jsweet.org */
-var hr;
-(function (hr) {
-    class Action {
-    }
-    hr.Action = Action;
-    Action["__class"] = "hr.Action";
-    (function (Action) {
-        class User {
-            constructor() {
-                this.id = 22;
-            }
+"use strict";
+class Action {
+}
+exports.Action = Action;
+Action["__class"] = "hr.Action";
+(function (Action) {
+    class User {
+        constructor() {
+            this.id = 22;
         }
-        Action.User = User;
-        User["__class"] = "hr.Action.User";
-    })(Action = hr.Action || (hr.Action = {}));
-})(hr || (hr = {}));
+    }
+    Action.User = User;
+    User["__class"] = "hr.Action.User";
+})(Action = exports.Action || (exports.Action = {}));
